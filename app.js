b0057f5851ed97f72864ec39977ddbe82a865202
IslamHegazy7196 <islamfathihegazy04@gmai.com>
2022-05-06 07:01:42 +0200

    basic express setup

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('E-Commerce API');
+const express=require('express')
+const app =express()
+
+
+app.route('/',(req,res)=>{
+    res.send('111111111')
+})
+
+const port=process.env.PORT ||5000
+
+const start=async()=>{
+    try {
+        app.listen(port,()=>{console.log(`server is listening on port ${port}`)})
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
